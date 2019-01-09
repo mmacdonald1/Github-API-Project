73be4570893b7bff51f326206031b2765836b0a8
mmacdonald1 <melmacd94@gmail.com>
2019-01-08 23:37:01 -0500

    profile styling

--- src/components/UserProfile.js
+++ src/components/UserProfile.js
@@ -1,20 +1,24 @@
 import React from "react";
-import { Card, CardTitle, CardText, Button, CardImg } from 'reactstrap';
+import { Card, CardTitle, CardText, Button, CardImg, Row, Col } from 'reactstrap';
 
 
 const UserProfile = (props) => {
   const {name, email, publicRepos, username, githubProfile, img} = props
    return(
-     <div>
-        <Card body>
-          <CardImg top width="100%" src={img} alt="Card image cap" />
-          <br />
-          <CardTitle>Name: {name}</CardTitle>
-          <CardText>Username: {username}</CardText>
-          <CardText>Email: {email}</CardText>
-          <CardText>Number of Public Repos: {publicRepos}</CardText>
-          <Button href={githubProfile}>Go to {username}'s Github </Button>
-        </Card>
+     <div className="profile-container" >
+        <Row className="profile-container">
+          <Col sm="10" className="profile-container">
+            <Card body>
+              <CardImg top width="100%" src={img} alt="Card image cap" />
+              <br />
+              <CardTitle>Name: {name}</CardTitle>
+              <CardText>Username: {username}</CardText>
+              <CardText>Email: {email}</CardText>
+              <CardText>Number of Public Repos: {publicRepos}</CardText>
+              <Button href={githubProfile}>Go to {username}'s Github </Button>
+            </Card>
+           </Col>
+         </Row>
      </div>
    )
 }
